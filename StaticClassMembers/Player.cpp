511dfc1acfd6af357760365ac12093d625cd452b
Kristóf Kovács <94800908+Krizsi96@users.noreply.github.com>
2022-10-22 09:57:55 +0200

    add static 'player_numbers' variable and static 'get_num_players' function

--- StaticClassMembers/Player.cpp
+++ StaticClassMembers/Player.cpp
@@ -1,10 +1,16 @@
 #include "Player.h"
 
+int Player::num_players{0};
+
 Player::Player(std::string const name_val, int const health_val,
                int const xp_val)
-    : name{name_val}, health{health_val}, xp{xp_val} {}
+    : name{name_val}, health{health_val}, xp{xp_val} {
+  ++num_players;
+}
 
 Player::Player(const Player &source)
     : Player{source.name, source.health, source.xp} {}
 
-Player::~Player() {}
+Player::~Player() { --num_players; }
+
+int Player::get_num_players() { return num_players; }
--- StaticClassMembers/Player.h
+++ StaticClassMembers/Player.h
@@ -4,6 +4,7 @@
 
 class Player {
  private:
+  static int num_players;
   std::string name;
   int health;
   int xp;
@@ -12,11 +13,14 @@ class Player {
   std::string get_name() const { return name; }
   int get_health() const { return health; }
   int get_xp() const { return xp; }
-  Player(std::string name_val = "None", int health_val = 0, int xp_val = 0);
+  explicit Player(std::string name_val = "None", int health_val = 0,
+                  int xp_val = 0);
   // Copy constructor
   Player(const Player &source);
   // Destructor
   ~Player();
+
+  static int get_num_players();
 };
 
 #endif  // _PLAYER_H_
