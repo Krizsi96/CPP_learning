a3db02183da70ce4230b0eac89ea9e6da0b5b096
Kristóf Kovács <94800908+Krizsi96@users.noreply.github.com>
2022-10-22 10:12:52 +0200

    finish up the class

--- StaticClassMembers/main.cpp
+++ StaticClassMembers/main.cpp
@@ -6,14 +6,26 @@
 
 using namespace std;
 
-void display_active_player() {
+void display_active_players() {
   cout << "Active players: " << Player::get_num_players() << endl;
 }
 
 int main() {
-  display_active_player();
+  display_active_players();
   Player hero{"Hero"};
-  display_active_player();
+  display_active_players();
+
+  {
+    Player frank{"Frank"};
+    display_active_players();
+  }
+
+  display_active_players();
+
+  Player *enemy = new Player("Enemy", 100, 100);
+  display_active_players();
+  delete enemy;
+  display_active_players();
 
   return 0;
 }
