b2859bcd33cd3798763379c92a1da43db32dc2b0
Kristóf Kovács <94800908+Krizsi96@users.noreply.github.com>
2022-10-22 09:25:31 +0200

    create Player class in header file

--- StaticClassMembers/Player.h
+++ StaticClassMembers/Player.h
@@ -2,4 +2,21 @@
 #define _PLAYER_H_
 #include <string>
 
+class Player {
+ private:
+  std::string name;
+  int health;
+  int xp;
+
+ public:
+  std::string get_name() const { return name; }
+  int get_health() const { return health; }
+  int get_xp() const { return xp; }
+  Player(std::string name_val = "None", int health_val = 0, int xp_val = 0);
+  // Copy constructor
+  Player(const Player &source);
+  // Destructor
+  ~Player();
+};
+
 #endif  // _PLAYER_H_
